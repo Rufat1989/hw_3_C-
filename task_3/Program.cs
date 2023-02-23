f584c111257e29e43947c32bb8f9658c35b4daf4
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-23 15:52:56 +0600

    third task

---
+++ task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int s1 = 1;
+
+
+while(s1 <= n)
+{
+    Console.Write ( $"{Math.Pow (s1,3)}  ");
+    s1 ++;
+}
