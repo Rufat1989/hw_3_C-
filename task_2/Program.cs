a8e0c7237ed6d1a901dbd772a53c405ee9a28d8d
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-23 15:35:11 +0600

    second task

---
+++ task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите координаты первой точки ([x,y,z])");
+double x1 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+double z1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки ([x,y,z])");
+double x2 = double.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
+double z2 = double.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2) ); 
+
+Console.Write($"result={result:f2}");
+ 
