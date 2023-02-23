878bc7b25cd950b91b6bd85ed5daf7ba4eef8755
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-23 15:19:58 +0600

    first task

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine());
+int x = n/1000;
+int y = n%100;
+int z = n/100;
+int[] array = {x/10, x%10, z%10, y/10, y%10};
+
+if(array[0] == array[4] && array[1] == array[3])
+{
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
